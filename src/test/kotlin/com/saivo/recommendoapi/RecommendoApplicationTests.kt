a22a392b1c6e930356bd2deaaab1d245f55a46f0
Ian Mubangizi <io@ianmubangizi.com>
2019-07-22 13:00:45 +0200

    Commit Spring Project

---
+++ src/test/kotlin/com/saivo/recommendoapi/RecommendoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.saivo.recommendoapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RecommendoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
