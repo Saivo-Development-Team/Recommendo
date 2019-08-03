28f4f995443a9091d5c759df10687b3496fc234d
Ian Mubangizi <io@ianmubangizi.com>
2019-08-03 17:45:49 +0200

    Changing Spring Config & Project Setup

---
+++ src/main/kotlin/com/saivo/recommendo/RecommendoApplication.kt
@@ -0,0 +1,11 @@
+package com.saivo.recommendo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecommendoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecommendoApplication>(*args)
+}
---
+++ src/test/kotlin/com/saivo/recommendo/RecommendoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.saivo.recommendo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecommendoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
