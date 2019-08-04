0ec20225c5481d173ec19dcb392d2cea8b198d5d
Ian Mubangizi <io@ianmubangizi.com>
2019-08-04 15:05:36 +0200

    Remote Dev Tested Passed More to Staging

--- src/main/resources/schema-production.sql
+++ src/main/resources/schema-production.sql
@@ -6,25 +6,27 @@ alter schema public owner to saivo_admin;
 
 create table if not exists activities
 (
-    id varchar(255) not null
+    id          varchar(255) not null
         constraint activities_pkey
             primary key,
     description varchar(255),
-    location varchar(255),
-    title varchar(255)
+    location    varchar(255),
+    title       varchar(255)
 );
 
-alter table activities owner to saivo_admin;
+alter table activities
+    owner to saivo_admin;
 
 create table if not exists preferences
 (
-    id varchar(255) not null
+    id    varchar(255) not null
         constraint preferences_pkey
             primary key,
     likes varchar(255)
 );
 
-alter table preferences owner to saivo_admin;
+alter table preferences
+    owner to saivo_admin;
 
 create table if not exists ratings
 (
@@ -33,11 +35,12 @@ create table if not exists ratings
             primary key
 );
 
-alter table ratings owner to saivo_admin;
+alter table ratings
+    owner to saivo_admin;
 
 create table if not exists recommendations
 (
-    id varchar(255) not null
+    id          varchar(255) not null
         constraint recommendations_pkey
             primary key,
     activity_id varchar(255)
@@ -45,41 +48,44 @@ create table if not exists recommendations
             references activities
 );
 
-alter table recommendations owner to saivo_admin;
+alter table recommendations
+    owner to saivo_admin;
 
 create table if not exists roles
 (
-    id varchar(255) not null
+    id   varchar(255) not null
         constraint roles_pkey
             primary key,
     role varchar(255)
 );
 
-alter table roles owner to saivo_admin;
+alter table roles
+    owner to saivo_admin;
 
 create table if not exists users
 (
-    id varchar(255) not null
+    id                  varchar(255) not null
         constraint users_pkey
             primary key,
-    password varchar(255),
-    username varchar(255) not null
+    password            varchar(255),
+    username            varchar(255) not null
         constraint const_user_username
             unique,
-    account_expired boolean not null,
-    account_locked boolean not null,
-    credentials_expired boolean not null,
-    email varchar(255),
-    enabled boolean not null,
-    firstname varchar(255),
-    lastname varchar(255)
+    account_expired     boolean      not null,
+    account_locked      boolean      not null,
+    credentials_expired boolean      not null,
+    email               varchar(255),
+    enabled             boolean      not null,
+    firstname           varchar(255),
+    lastname            varchar(255)
 );
 
-alter table users owner to saivo_admin;
+alter table users
+    owner to saivo_admin;
 
 create table if not exists users_preferences
 (
-    user_id varchar(255) not null
+    user_id        varchar(255) not null
         constraint const_users_preferences
             references users,
     preferences_id varchar(255) not null
@@ -91,11 +97,12 @@ create table if not exists users_preferences
         primary key (user_id, preferences_id)
 );
 
-alter table users_preferences owner to saivo_admin;
+alter table users_preferences
+    owner to saivo_admin;
 
 create table if not exists users_recommendations
 (
-    user_id varchar(255) not null
+    user_id            varchar(255) not null
         constraint con_user_id
             references users,
     recommendations_id varchar(255) not null
@@ -107,11 +114,12 @@ create table if not exists users_recommendations
         primary key (user_id, recommendations_id)
 );
 
-alter table users_recommendations owner to saivo_admin;
+alter table users_recommendations
+    owner to saivo_admin;
 
 create table if not exists users_roles
 (
-    user_id varchar(255) not null
+    user_id  varchar(255) not null
         constraint const_user_id
             references users,
     roles_id varchar(255) not null
@@ -123,81 +131,88 @@ create table if not exists users_roles
         primary key (user_id, roles_id)
 );
 
-alter table users_roles owner to saivo_admin;
+alter table users_roles
+    owner to saivo_admin;
 
 create table if not exists oauth_client_details
 (
-    client_id varchar(256) not null
+    client_id               varchar(256) not null
         constraint oauth_client_details_pkey
             primary key,
-    resource_ids varchar(256),
-    client_secret varchar(256),
-    scope varchar(256),
-    authorized_grant_types varchar(256),
+    resource_ids            varchar(256),
+    client_secret           varchar(256),
+    scope                   varchar(256),
+    authorized_grant_types  varchar(256),
     web_server_redirect_uri varchar(256),
-    authorities varchar(256),
-    access_token_validity integer,
-    refresh_token_validity integer,
-    additional_information varchar(4096),
-    autoapprove varchar(256)
+    authorities             varchar(256),
+    access_token_validity   integer,
+    refresh_token_validity  integer,
+    additional_information  varchar(4096),
+    autoapprove             varchar(256)
 );
 
-alter table oauth_client_details owner to saivo_admin;
+alter table oauth_client_details
+    owner to saivo_admin;
 
 create table if not exists oauth_client_token
 (
-    token_id varchar(256),
-    token bytea,
+    token_id          varchar(256),
+    token             bytea,
     authentication_id varchar(256) not null
         constraint oauth_client_token_pkey
             primary key,
-    user_name varchar(256),
-    client_id varchar(256)
+    user_name         varchar(256),
+    client_id         varchar(256)
 );
 
-alter table oauth_client_token owner to saivo_admin;
+alter table oauth_client_token
+    owner to saivo_admin;
 
 create table if not exists oauth_access_token
 (
-    token_id varchar(256),
-    token bytea,
+    token_id          varchar(256),
+    token             bytea,
     authentication_id varchar(256) not null
         constraint oauth_access_token_pkey
             primary key,
-    user_name varchar(256),
-    client_id varchar(256),
-    authentication bytea,
-    refresh_token varchar(256)
+    user_name         varchar(256),
+    client_id         varchar(256),
+    authentication    bytea,
+    refresh_token     varchar(256)
 );
 
-alter table oauth_access_token owner to saivo_admin;
+alter table oauth_access_token
+    owner to saivo_admin;
 
 create table if not exists oauth_refresh_token
 (
-    token_id varchar(256),
-    token bytea,
+    token_id       varchar(256),
+    token          bytea,
     authentication bytea
 );
 
-alter table oauth_refresh_token owner to saivo_admin;
+alter table oauth_refresh_token
+    owner to saivo_admin;
 
 create table if not exists oauth_code
 (
-    code varchar(256),
+    code           varchar(256),
     authentication bytea
 );
 
-alter table oauth_code owner to saivo_admin;
+alter table oauth_code
+    owner to saivo_admin;
 
 create table if not exists oauth_approvals
 (
-    userid varchar(256),
-    clientid varchar(256),
-    scope varchar(256),
-    status varchar(10),
-    expiresat timestamp,
+    userid         varchar(256),
+    clientid       varchar(256),
+    scope          varchar(256),
+    status         varchar(10),
+    expiresat      timestamp,
     lastmodifiedat timestamp
 );
 
-alter table oauth_approvals owner to saivo_admin;
+alter table oauth_approvals
+    owner to saivo_admin;
 
