18d1fa97a38903ab9d4dbe4840b25c5a39adff24
Ian Mubangizi <io@ianmubangizi.com>
2019-08-18 22:03:07 +0200

    Taking use of the Response Object

--- src/main/kotlin/com/saivo/recommendo/controller/api/UserController.kt
+++ src/main/kotlin/com/saivo/recommendo/controller/api/UserController.kt
@@ -2,6 +2,7 @@ package com.saivo.recommendo.controller.api
 
 import com.saivo.recommendo.model.objects.Login
 import com.saivo.recommendo.model.domain.User
+import com.saivo.recommendo.model.objects.Response
 import com.saivo.recommendo.service.UserService
 import com.saivo.recommendo.util.exception.UserNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,25 +18,24 @@ class UserController {
     fun getUsers(): List<User> = userService!!.getUsers()
 
     @GetMapping("/api/users/{id}")
-    fun getUser(@PathVariable id: String) : User {
+    fun getUser(@PathVariable id: String): Response {
         return try {
-            userService!!.getUserById(id)
+            Response(data = userService!!.getUserById(id), status = "USER_FOUND")
         } catch (e: UserNotFoundException) {
-            print(e.message)
-            e.message
-        } as User
+            Response(error = "USER_NOT_FOUND", status = "USER_ERROR", message = e.message!!)
+        }
     }
 
     @ResponseBody
     @PutMapping("/api/users/{id}")
     fun updateUser(@PathVariable id: String, @RequestBody user: User):
-            String? = userService!!.saveUser(user, "update")
+            Response = userService!!.saveUser(user, "update")
 
     @PostMapping("/users/register")
     @PreAuthorize("#oauth2.hasScope('register')")
-    fun registerUser(@RequestBody user: User): String? = userService!!.saveUser(user)
+    fun registerUser(@RequestBody user: User): Response = userService!!.saveUser(user)
 
     @PostMapping("/users/login")
-    fun loginUser(@RequestBody login: Login): User? = userService!!.loginUser(login)
+    fun loginUser(@RequestBody login: Login): Response = userService!!.loginUser(login)
 
 }
