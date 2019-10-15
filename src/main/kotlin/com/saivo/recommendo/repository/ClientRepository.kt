c958709b9469b54f729bbc7415a270bd9b6a887e
Ian Mubangizi <io@ianmubangizi.com>
2019-10-15 12:27:47 +0200

    Remove useless annotation

--- src/main/kotlin/com/saivo/recommendo/repository/ClientRepository.kt
+++ src/main/kotlin/com/saivo/recommendo/repository/ClientRepository.kt
@@ -2,7 +2,5 @@ package com.saivo.recommendo.repository
 
 import com.saivo.recommendo.model.infrastructure.Client
 import org.springframework.data.repository.CrudRepository
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
 interface ClientRepository : CrudRepository<Client, String>
--- src/main/kotlin/com/saivo/recommendo/repository/ProfileImageRepository.kt
+++ src/main/kotlin/com/saivo/recommendo/repository/ProfileImageRepository.kt
@@ -4,5 +4,4 @@ import com.saivo.recommendo.model.objects.ProfileImage
 import org.springframework.data.repository.CrudRepository
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
 interface ProfileImageRepository: CrudRepository<ProfileImage, String>
